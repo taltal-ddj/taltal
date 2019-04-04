d8e41fab2343c3165d3be384e82c6ae39b8a41e5
Yewoon Bae <moonjy@khu.ac.kr>
2019-04-04 14:24:41 +0900

    test version

--- R/highofficer.R
+++ R/highofficer.R
@@ -4,23 +4,6 @@
 #'
 #' @source https://www.transtats.bts.gov/DL_SelectFields.asp?Table_ID=236
 #' @format Tibble with columns
-#' \describe{
-#' \item{carrier}{Two letter abbreviation}
-#' \item{name}{Full name}
-#' \item{carrier}{Two letter abbreviation}
-#' \item{carrier}{Two letter abbreviation}
-#' \item{carrier}{Two letter abbreviation}
-#' \item{carrier}{Two letter abbreviation}
-#' \item{carrier}{Two letter abbreviation}
-#' \item{carrier}{Two letter abbreviation}
-#' \item{name}{Full name}
-#' \item{carrier}{Two letter abbreviation}
-#' \item{carrier}{Two letter abbreviation}
-#' \item{carrier}{Two letter abbreviation}
-#' \item{carrier}{Two letter abbreviation}
-#' \item{carrier}{Two letter abbreviation}
-#' \item{carrier}{Two letter abbreviation}
-#' \item{carrier}{Two letter abbreviation}
 #'
 #' }
 #' @examples
